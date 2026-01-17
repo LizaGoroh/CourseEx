1920a5dbe5f515f907a99be74fcbe3956969855a
LizaGoroh <138466766+LizaGoroh@users.noreply.github.com>
2026-01-17 19:11:11 +0300

    Add files via upload

---
+++ 1.cs
@@ -0,0 +1,17 @@
+﻿{
+    int health;
+    int armor;
+    int damage;
+    int percentConverter = 100;
+
+    Console.Write("Amount of health: ");
+    health = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Amount of armor: ");
+    armor = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Amount of damage: ");
+    damage = Convert.ToInt32(Console.ReadLine());
+
+    health -= damage * armor / percentConverter;
+
+    Console.WriteLine($"You`ve got {damage} damage. You have left {health} health.");
+}
